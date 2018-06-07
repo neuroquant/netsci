3f061c958e0c367d8aedb6d49e76bfb5887e3c51
mnarayan <mnarayan@users.noreply.github.com>
2018-06-06 23:41:14 -0700

    Modified setup

--- setup.m
+++ setup.m
@@ -1,8 +1,16 @@
 if(exist('lib/matlab-library','dir'))
-	addpath(genpath('lib/matlab-library/lib/'));
-	addpath(genpath('lib/matlab-library/test/'));
+	addpath('lib/matlab-library/lib/');
+	addpath('lib/matlab-library/test/');
 else
 	error('matlab-library not installed at lib/')
 end
 
-	
+addpath('netsci');
+addpath(fullfile('netsci','utils')); 
+
+import +community.*
+import +tda.*
+
+addpath('external/matlab-cliquer/')
+addpath('external/k-clique/')
+addpath('~/MATLAB/packages/matlab-bgl');
