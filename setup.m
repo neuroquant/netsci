9529c255a0ddff6a069f1aec74178e6400fb262d
mnarayan <mnarayan@users.noreply.github.com>
2018-06-11 17:52:31 -0700

    Misc. changes

--- setup.m
+++ setup.m
@@ -1,16 +1,23 @@
-if(exist('lib/matlab-library','dir'))
-	addpath('lib/matlab-library/lib/');
-	addpath('lib/matlab-library/test/');
+% Be sure to install github.com:TheEtkinLab/matlab-library in the lib/ directory. 
+
+MATLABLIB='lib/matlab-library';
+
+if(exist(MATLABLIB,'dir'))
+	addpath(fullfile(MATLABLIB,'lib'));
+	addpath(fullfile(MATLABLIB,'test'));
+    addpath(fullfile(MATLABLIB,'lib','BrewerMap'));
 else
 	error('matlab-library not installed at lib/')
 end
 
 addpath('netsci');
+addpath('data');
 addpath(fullfile('netsci','utils')); 
+addpath(fullfile('netsci','plot')); 
 
 import +community.*
 import +tda.*
 
 addpath('external/matlab-cliquer/')
-addpath('external/k-clique/')
-addpath('~/MATLAB/packages/matlab-bgl');
+% addpath('external/k_clique/')
+% addpath('~/MATLAB/packages/matlab-bgl');
